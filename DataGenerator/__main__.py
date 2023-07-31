688909bfad9a8efc54d18ee84f25f03038aac406
isabelabunescu <biancabelaisabela@yahoo.com>
2023-08-01 00:19:07 +0200

    updated endpoints

--- DataGenerator/__main__.py
+++ DataGenerator/__main__.py
@@ -1,12 +1,18 @@
-import pymongo 
+from . import CountryData
+from . import ImaginaryTree
+from . import CountryDataModified
+import pymongo
+import datetime
 
 client = pymongo.MongoClient('localhost', 27017)
 
-from . import CountryData 
 CountryData.parse_and_write(client)
 
-from . import CountryDataModified
 CountryDataModified.parse_and_write(client)
 
-from . import ImaginaryTree
 ImaginaryTree.parse_and_write(client)
+
+db = client['TreeMap']
+index_users = db["Users"]
+index_users.insert_one({'key': 'root_key',  'name': 'Default root user',  'root': True,  'edit': True,
+                       'creation_date': str(datetime.datetime.now()),  'last_access': 'never'})
--- flaskServer.py
+++ flaskServer.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas
- 
+import datetime 
+
 import pymongo 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +11,7 @@ client = pymongo.MongoClient('localhost', 27017)
 db = client['TreeMap']
 index_collection = db["TreeDataIndex"]
 data_collection = db['TreeData']
+users_collection = db['Users']
 
 @app.route('/data/json/<name>', methods=['GET', 'POST'])
 def generate_json(name):
@@ -19,6 +21,12 @@ def generate_json(name):
 
 @app.route('/data/index', methods=['GET'])
 def get_index():
+    """
+    Get all the Index entres from the database and sends them to the clinet.
+
+    Returns:
+        json: the json containing the list of entries.
+    """
     lst = []
     for document in index_collection.find():
         lst.append({'name' : document['name'], 'display_name' : document['display_name'], 'description' : document['description']})
@@ -39,6 +47,8 @@ def update_index():
         return jsonify({"success": True})
     else:
         return jsonify({"success": False})
+    
+
 
 
 @app.route('/data/delete/<name>', methods=['DELETE'])
@@ -48,5 +58,47 @@ def delete_entry(name):
 
     return "", 204
     
+@app.route('/users/delete/<key>', methods=['DELETE'])
+def delete_user(key):
+    print(key, users_collection.count_documents({'key' : key}, limit = 1))
+    if users_collection.count_documents({'key' : key}, limit = 1) > 0:
+        users_collection.delete_many({'key': key})
+        return 'Done', 204
+    else:
+        return 'Not found', 404
+
+@app.route('/users/list', methods=['GET'])
+def get_users():
+    lst = []
+    for document in users_collection.find():
+        lst.append({'key': document['key'],  'name': document['name'],  'root': document['root'],  'edit': document['edit'],
+                       'creation_date': document['creation_date'],  'last_access': document['last_access']})
+    return jsonify(lst)
+
+@app.route('/users/add', methods=['PUT'])
+def add_user():
+    try:
+        key = str(request.json['key'])
+        name = str(request.json['name'])
+        root = bool(request.json['root'])
+        edit = bool(request.json['edit'])
+        if len(key) < 6 or not key.isalnum():
+            return jsonify({"success": False, "message": "key must contain at least 6 characters (a-z, A-Z, 0-9)"})
+        if len(name) < 3 :
+            return jsonify({"success": False, "message": "name 3 characters"})
+        if root:
+            edit = True
+    except:
+        return jsonify({"success": False, "message": "Invalid form data"})
+    
+    if users_collection.count_documents({'key' : key}, limit = 1) > 0:
+        #update
+        users_collection.update_one({'name' : name}, {"$set": {'key' : key, 'name' : name, 'root' : root, 'edit': edit, 'date_created': str(datetime.datetime.now), 'last_accessed': 'never' }})
+        return jsonify({"success": True, "message": "User updated"})
+    else:
+        users_collection.insert_one({'key' : key, 'name' : name, 'root' : root, 'edit': edit,
+                       'creation_date': str(datetime.datetime.now()),  'last_access': 'never'})
+        return jsonify({"success": True, "message": "User created"})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
